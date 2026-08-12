c1475936e73031d4d1b97a2d0d045e7477d4e6df
gizemhazalakbulut <gizemhazalakbulut123@gmail.com>
2026-08-12 16:03:40 +0300

    3.proje

---
+++ NetCoreAINew.Project03_HuggingFaceSummarizeText/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
+
+Console.Write("Enter your text here: ");
+
+var apiKey = "";
+var inputText = Console.ReadLine();
+
+var requestData = new
+{
+    inputs = inputText
+};
+
+var json = JsonSerializer.Serialize(requestData); // requestData nesnesini JSON formatına dönüştür
+var content = new StringContent(json, Encoding.UTF8, "application/json"); // JSON verisini HTTP isteği gövdesine ekle
+
+using var client = new HttpClient();
+client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", apiKey);
+
+//var response = await client.PostAsync("https://api-inference.huggingface.co/models/facebook/bart-large-cnn", content);
+var response = await client.PostAsync("https://api-inference.huggingface.co/models/sshleifer/distilbart-cnn-12-6", content);
+var responseContent = await response.Content.ReadAsStringAsync();
+
+Console.WriteLine("🗒️ Text Summarize: ");
+Console.WriteLine(responseContent);
